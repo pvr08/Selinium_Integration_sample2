d152432d9fc8c95b41446387b30f03bec01dc555
pvr08 <113646080+pvr08@users.noreply.github.com>
2024-02-19 15:17:16 -0500

    Update TestRunnerTest.java

--- src/test/java/StepDefinitions/TestRunnerTest.java
+++ src/test/java/StepDefinitions/TestRunnerTest.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		
-	features="src/test/resources/Features/vamshi.feature", //file name to be executed should go here( can be multiple as array)
+	features="src/test/resources/Features/googleSearch.feature", //file name to be executed should go here( can be multiple as array)
 	glue= {"StepDefinitions","pages"}, // where are our step definitions are present
 	monochrome = true,
 	plugin = {"pretty","html:test-output/HtmlReports"}
